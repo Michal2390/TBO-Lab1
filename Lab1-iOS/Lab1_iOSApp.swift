f351ff391d4378e01496c44e388a2a02528ace50
Michał Fereniec <michalkoks99@gmail.com>
2026-01-29 17:28:40 +0100

    Initial Commit

---
+++ Lab1-iOS/Lab1_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_iOSApp.swift
+//  Lab1-iOS
+//
+//  Created by Michal Fereniec on 29/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab1-iOSTests/Lab1_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_iOSTests.swift
+//  Lab1-iOSTests
+//
+//  Created by Michal Fereniec on 29/01/2026.
+//
+
+import Testing
+@testable import Lab1_iOS
+
+struct Lab1_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
